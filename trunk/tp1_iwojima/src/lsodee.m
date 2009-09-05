e1d8388cc41162c1c497b6adb0ef8e867c2b764c
albertopose <albertopose@8ad82c64-9a37-11de-a465-5fc2cdd6756b>
2009-09-05 16:38:18 +0000

    Initial commit.

---
+++ trunk/tp1_iwojima/src/lsodee.m
@@ -0,0 +1,21 @@
+# lsodee es la función que representa la ecuación diferencial que se
+# va a integrar.
+#
+# X es el vector que contiene x1 y x2
+# Xpunto es el vector que contiene xpunto1 y xpunto2
+#
+function Xpunto = lsodee(X, t)
+    global a
+    global b
+    global c21
+    global c12
+    global R
+    x1 = X(1); x2 = X(2);
+    r1 = R(1,:); 
+    r2 = R(2,:);
+    r1res = feval(r1, t);
+    r2res = feval(r1, t);
+    xpunto1 = -a * x1 - c21 * x2 + r1res;
+    xpunto2 = -b * x2 - c12 * x1 + r2res;
+    Xpunto = [xpunto1 xpunto2];
+endfunction 
