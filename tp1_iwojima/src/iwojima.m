b8568672a3c8aec84af959a64f9d6f1e412eb68e
albertopose <albertopose@8ad82c64-9a37-11de-a465-5fc2cdd6756b>
2009-09-05 21:12:49 +0000

    Documentation improvement

--- tp1_iwojima/src/iwojima.m
+++ tp1_iwojima/src/iwojima.m
@@ -5,20 +5,22 @@ global b
 global c21
 global c12
 
-# Item a)
-# Despreciando las tasas unitarias de perdidas operativas:
+# Despreciamos las tasas unitarias de perdidas operativas:
 a = 0; b = 0;
 
+# Coeficientes de efectividad en combate de la fuerza enemiga
+c12 = .0544;    # "Daño efectivo" que le hace la fuerza 1 a la fuerza 2
+c21 = .0106;    # "Daño efectivo" que le hace la fuerza 2 a la fuerza 1
 
-# demostrar que existe una constante K tal que:
-#
-#  c21 x^2 - c12 y^2 = K
-#
+# Fuerza 1: Japón
+# Fuerza 2: EEUU
 
+[t x1 x2] = combat_simulator(
+        [21500,0],              #Cantidad de fuerzas en combate: ej: [japon usa]
+        36,                     #duración en días de la batalla
+        "zeroreinforcement",    #política de refuerzos de la primera fuerza
+        "reinforcement");       #política de refuerzos de la segunda fuerza
 
-c12 = .0544;
-c21 = .0106;
-
-[t x1 x2] = combat_simulator([21500,0], 36.4, "zeroreinforcement", "reinforcement");
-
-res = [t, x1, x2]
+res = [t,                       #días simulados
+        x1,                     #cantidad de tropas primera fuerza
+        x2]                     #cantidad de tropas segunda fuerza
