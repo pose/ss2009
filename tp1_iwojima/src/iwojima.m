d913bc8435898241ecbfc0f326a2d9980311a577
albertopose <albertopose@8ad82c64-9a37-11de-a465-5fc2cdd6756b>
2009-09-16 18:21:01 +0000

    Agrego script para generar nuevas imagenes

--- tp1_iwojima/src/iwojima.m
+++ tp1_iwojima/src/iwojima.m
@@ -31,6 +31,15 @@ japan_vs_usa.t = t;
 japan_vs_usa.japan = x1;
 japan_vs_usa.usa = x2;
 
+global last_figure;
+last_figure = 0;
+
+function n=next_figure()
+    global last_figure;
+    n=++last_figure;
+    figure(n);
+endfunction
+
 # --------------------------------------------------
 # --------------------------------------------------
 #                       Punto (a)
@@ -40,11 +49,62 @@ japan_vs_usa.usa = x2;
 # (incluir mi ecuación)
 # El valor de K, como se puede observar es una constante de integración. 
 # Depende de las condiciones iniciales, en este caso particular es la diferencia
-# de tropas entre una fuerza y otra. En el caso que sea negativa, quiere decir 
-# que la fuerza caracterizada por x (???? No sé, estoy en blanco mal)
+# de tropas entre una fuerza y otra.
 #
 #
+# Dejamos y en función de x:
 #
+# alpha * y^2 - beta * x^2 = K
+#
+# y^2 = (K + beta * x^2) / alpha
+#
+# y = sqrt( K + beta * x^2 ) / alpha
+#
+
+#alpha = 0.01;
+#beta = 0.04;
+#K = [0,5000,10000,-10000,-5000];
+#s = '-+^*ox';
+
+function draw_k(x, alpha, beta, K, s)
+    
+    for i=1:length(K)
+        if K(i) != 0
+            y = real(sqrt((K(i) + beta * x.*x) / alpha ));
+        else
+            y = real(sqrt(beta/alpha) * x);
+        endif
+        plot (x, y, sprintf('%c;K = %d;',s(i),K(i)));
+        hold on
+    endfor
+
+    legend({},"location","southeast")
+    legend("boxon")
+    hold off
+
+
+    xlabel (sprintf("{/Helvetica}Efectivos de (1) \n {/Symbol a } = %g",alpha));
+    ylabel (sprintf("{/Helvetica}Efectivos de (2) \n {/Symbol b } = %g",beta));
+    t = title (['{/Helvetica}Efectos de la variaci',243,'n de K']);
+
+    grid("on");
+
+endfunction
+
+next_figure();
+draw_k(linspace(0,2500,50),0.01, 0.04, [0,10000,20000,-10000,-20000], '-+^*ox');
+print -dpng power_areas_1.png 
+next_figure();
+draw_k(linspace(0,2500,50),0.01, 0.01, [0,10000,20000,-20000,-10000], '-+^*ox');
+print -dpng power_areas_2.png 
+next_figure();
+draw_k(logspace(0,3.85,25),0.01, 0.04, [0,25000,50000,-50000,-25000], '-+^*ox');
+print -dpng power_areas_1b.png 
+next_figure();
+draw_k(logspace(0,3.85,25),0.01, 0.01, [0,25000,50000,-50000,-25000], '-+^*ox');
+print -dpng power_areas_2b.png 
+
+
 
 # --------------------------------------------------
 # --------------------------------------------------
@@ -55,7 +115,7 @@ japan_vs_usa.usa = x2;
 # --------------------------------------------------
 # Gráfico de x(t) e y(t) versus t
 # --------------------------------------------------
-figure(1);
+next_figure();
 
 plot (japan_vs_usa.t, japan_vs_usa.japan,strcat("o;Fuerzas del Imperio del Jap",sprintf("%c",243),"n;"), japan_vs_usa.t, japan_vs_usa.usa,strcat("*;Fuerzas de Estados Unidos;"));
 title (strcat("Simulaci",sprintf("%c",243),"n num",sprintf("%c",233),"rica de la batalla de Iwo Jima"));
@@ -70,7 +130,7 @@ print -dpng jap_vs_usa.png
 # --------------------------------------------------
 # Gráfico de fuerzas norteamericanas reales contra simuladas
 # --------------------------------------------------
-figure(2);
+next_figure();
 
 plot(japan_vs_usa.t, japan_vs_usa.usa,"*;Unidades simuladas;",[7,9,12,19,24],[66000, 62000, 58500, 56200, 54000],strcat("+;Unidades hist",sprintf("%c",243),"ricas;"),"markersize",10);
 
@@ -112,7 +172,7 @@ reinforce2.t = t;
 reinforce2.japan = x1;
 reinforce2.usa = x2;
 
-figure(3);
+next_figure();
 
 plot (reinforce2.t, reinforce2.japan,strcat("o;Fuerzas del Imperio del Jap",sprintf("%c",243),"n;"), reinforce2.t, reinforce2.usa,"*;Fuerzas de Estados Unidos;");
 title (strcat("Pol",sprintf("%c",237),"tica de refuerzo alternativa (I) para el ej",sprintf("%c",233),"rcito de Estados Unidos"));
@@ -138,7 +198,7 @@ reinforce3.t = t;
 reinforce3.japan = x1;
 reinforce3.usa = x2;
 
-figure(4);
+next_figure();
 
 plot (reinforce3.t, reinforce3.japan, strcat("o;Fuerzas del Imperio del Jap",sprintf("%c",243),"n;"), reinforce3.t, reinforce3.usa,"*;Fuerzas de Estados Unidos;");
 title (strcat("Pol",sprintf("%c",237),"tica de refuerzo alternativa (II) para el ej",sprintf("%c",233),"rcito de Estados Unidos"));
@@ -165,7 +225,7 @@ reinforce4.t = t;
 reinforce4.japan = x1;
 reinforce4.usa = x2;
 
-figure(5);
+next_figure();
 
 plot (reinforce4.t, reinforce4.japan, strcat("o;Fuerzas del Imperio del Jap",sprintf("%c",243),"n;"), reinforce4.t, reinforce4.usa,"*;Fuerzas de Estados Unidos;");
 title (strcat("Pol",sprintf("%c",237),"tica de refuerzo alternativa (III) para el ej",sprintf("%c",233),"rcito de Estados Unidos"));
