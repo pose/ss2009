1437045eaf7aac1b485ea07b29934a94371ceca6
albertopose <albertopose@8ad82c64-9a37-11de-a465-5fc2cdd6756b>
2009-09-27 01:27:21 +0000

    Avances hasta ahora

---
+++ trunk/tp2_cinventario/src/deposit_simulator2.m
@@ -0,0 +1,32 @@
+#
+# Depot simulator
+# ---------------
+#
+#
+function [t2 x1 x2] = deposit_simulator(initial_units, sales_rate, trex)
+    global u;
+    global K;
+
+    
+    vopt = odeset ("RelTol", 1e-3, "AbsTol", 1e-3, "NormControl", "on",
+        "MaxStep",1, "Events", @zero_crossing, "InitialStep", 1e-1);
+#        "MaxStep",1, "InitialStep", 1e-1);
+    [t2, res] = ode45(@other_lsodee, linspace(0,trex,40), [initial_units, sales_rate], vopt);
+    
+    x1 = res(:,1);
+    x2 = res(:,2);
+    
+endfunction 
+
+
+function Xpunto = other_lsodee(t, X)
+    global u;
+    global K;
+
+    x1 = X(1); x2 = X(2);
+
+    xpunto1 = feval(u,t,x1) - x2;
+    xpunto2 = 6*x1 - K* feval(u,t,x1);
+
+    Xpunto = [xpunto1 xpunto2];
+endfunction
--- trunk/tp2_cinventario/src/home_depot.m
+++ trunk/tp2_cinventario/src/home_depot.m
@@ -19,6 +19,21 @@ s = '+o^*-';
 #for K=0.001:.0509:1
 k_plot.k_1.x = 0:5;
 
+do_three = true;
+do_two = true;
+
+if nargin == 1
+    args = argv();
+    if args{1} == '2'
+        do_three = false;
+    elseif args{1} == '3'
+        do_two = false;
+    endif
+endif
+
+
+if do_two
+
 for K=logspace(0.001,.5,5)-1
     [t units sales_rate] = deposit_simulator(20000, 100, 4);
     k_plot = setfield(k_plot, sprintf('k_%d', j),'x',t);
@@ -30,4 +45,30 @@ for K=logspace(0.001,.5,5)-1
 endfor
 poseplot(k_plot, 'k_plot.json');
 
-hold off
+endif
+
+
+if !do_three
+    return
+endif
+
+function y = my_r(t)
+    y = 100000;
+endfunction
+
+j = 1;
+k_plot2.k_1.x = 0:5;
+for K=logspace(0.001,.5,5)-1
+    [t units sales_rate] = deposit_simulator2(20000, 100, 19);
+    k_plot2 = setfield(k_plot2, sprintf('k_%d', j),'x',t);
+    k_plot2 = setfield(k_plot2, sprintf('k_%d', j),'y',units);
+    label = sprintf("K = %g",K);
+    k_plot2 = setfield(k_plot2, sprintf('k_%d', j),'label',label);
+    j++;
+    hold on
+endfor
+poseplot(k_plot2, 'k_plot2.json');
+
+
+
+
