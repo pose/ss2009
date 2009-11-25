2da79ffc83d0a3e715ebc29befd45d804c82891e
santiagovazq <santiagovazq@8ad82c64-9a37-11de-a465-5fc2cdd6756b>
2009-11-25 20:40:43 +0000

    dsfsdf

---
+++ trunk/tp4_modelodecola/src/parte3/arrive.m
@@ -0,0 +1,76 @@
+function arrive()
+% Funcion:   arrive
+% Proposito: Funcion de los eventos de arribos
+%
+%
+global Q_LIMIT;
+global BUSY;
+global IDLE;
+
+global next_event_type;
+global num_custs_delayed;
+global num_delays_required;
+global num_events;
+global num_in_q;
+global server_status1;
+global server_status2;
+
+global area_num_in_q;
+global area_server_status;
+global mean_interarrival;
+global mean_service1;
+global mean_service2;
+global time;
+global time_arrival;
+global time_last_event;
+global time_next_event;
+global total_of_delays;
+
+% Programa el proximo arrivo
+time_next_event(1) = time + expon(mean_interarrival);
+
+% Chequea si el servidor esta ocupado 
+if ( server_status1 == BUSY && server_status2 == BUSY)
+	% Si el servido esta ocupado: incrementa el numero de
+	% clientes en la cola.
+	 num_in_q = num_in_q + 1;
+
+	 % Chequea si no se excede la cantidad de clientes en cola
+	 if ( num_in_q > Q_LIMIT )
+	 	% La cola alcanzo su limite, entonces para la simulacion
+	 	fprintf(outfile, "\nOverflow de la cola a los %f minutos", time);
+		return;
+	end
+	% Hay lugar en la cola, entonces almacena el tiempo de llegada del
+	% cliente en arribo en el nuevo tiempo de arribo
+		time_arrival(num_in_q) = time;
+else
+	if (server_status1 == IDLE)
+	  % El servidor esta libre, entonces el cliente en arribo tiene un
+	  % delay igual a cero.
+	  delay            = 0.0;
+	  total_of_delays = total_of_delays + delay;
+
+	  % Incrementa el numero de clientes atendidos y pone el estatus
+	  % del servidor ocupado.
+	  num_custs_delayed = num_custs_delayed + 1;
+	  server_status1 = BUSY;
+	  
+	  % Programa una partida (servicio completado)
+	  time_next_event(2) = time + expon(mean_service1);
+	elseif (server_status2 == IDLE)
+	  % El servidor esta libre, entonces el cliente en arribo tiene un
+	  % delay igual a cero.
+	  delay            = 0.0;
+	  total_of_delays = total_of_delays + delay;
+
+	  % Incrementa el numero de clientes atendidos y pone el estatus
+	  % del servidor ocupado.
+	  num_custs_delayed = num_custs_delayed + 1;
+	  server_status2 = BUSY;
+	  
+	  % Programa una partida (servicio completado)
+	  time_next_event(3) = time + expon(mean_service2);
+	end
+end
+return;
---
+++ trunk/tp4_modelodecola/src/parte3/depart1.m
@@ -0,0 +1,56 @@
+function depart1()
+% Funcion:   depart
+% Proposito: Funcion de los eventos de partidas
+%
+%
+global Q_LIMIT;
+global BUSY;
+global IDLE;
+
+global next_event_type;
+global num_custs_delayed;
+global num_delays_required;
+global num_events;
+global num_in_q;
+global server_status1;
+global server_status2;
+
+global area_num_in_q;
+global area_server_status;
+global mean_interarrival;
+global mean_service1;
+global mean_service2;
+global time;
+global time_arrival;
+global time_last_event;
+global time_next_event;
+global total_of_delays;
+
+% Chequea si la cola esta vacia
+	if ( num_in_q == 0)
+		% La cola esta vacia entonces, pone el servidor en libre
+		server_status1      = IDLE;
+		time_next_event(2) = 1.0e+30;
+	else
+		% La cola no esta vacia, entonces decrementa el numero de
+		%  clientes en cola.
+		num_in_q = num_in_q - 1;
+		
+		% Calcula el delay del cliente que esta comenzando a ser servido
+		% y actualiza el acumulador del delay total
+		delay            = time - time_arrival(1);
+		total_of_delays = total_of_delays + delay;
+
+		% Incrementa el numero de clientes atendidos y programa la
+		%  partida.
+		num_custs_delayed = num_custs_delayed + 1;
+		time_next_event(2) = time + expon(mean_service1);
+
+		% Mueve cada cliente en la cola un lugar.
+		for i = 1:num_in_q
+			time_arrival(i) = time_arrival(i + 1);
+		end
+	end
+return;
+
+ 
