49a0fbe4b317f4fc384b107c23c1b4d85fe52602
albertopose <albertopose@8ad82c64-9a37-11de-a465-5fc2cdd6756b>
2009-11-26 14:20:57 +0000

    Subo mis cambios hasta el momento de la parte 2

---
+++ tp4_modelodecola/src/parte2/mm1.m
@@ -0,0 +1,125 @@
+% Matlab script: mm1.m
+% Proposito: Simula un modelo de cola con servidor simple del tipo
+%			  M/M/1 con disciplina FIFO.
+%			  Lee el archivo de entrada mm1.inp donde en el primer registro
+%			  aparecen, separados por espacios: tiempo medio entre arribos, 
+%			  tiempo medio de servicio y numero de clientes total procesados.
+%			  La salida de la simulacion se da en dos archivos:
+%			  mm1.out: Muestra valores estadisticos computados
+%			  mm1.tim: Muestra la series de tiempo resultado de la simulacion
+%			  
+%			  Adaptado de "Simulation Modeling & Analysis",
+%			  Law y Kelton.Proposito:     
+%				%Simula una cola M/M/1
+%
+% Funciones utilizadas:
+% arrive.m
+% depart.m
+% expon.m
+% initialize.m
+% report.m
+% timing.m
+% update_time_avg_stats.m
+%
+% Autor: Alejandro Diaz
+% Declaracion de constantes y variables:
+function [rho, relativeError, meanQueueLength] = mm1(lambda, mu, amount)
+
+%clear all
+
+global Q_LIMIT;
+global BUSY;
+global IDLE;
+
+global next_event_type;
+global num_custs_delayed;
+global num_delays_required;
+global num_events;
+global num_in_q;
+global server_status;
+
+global area_num_in_q;
+global area_server_status;
+global mean_interarrival;
+global mean_service;
+global time;
+global time_arrival;
+global time_last_event;
+global time_next_event;
+global total_of_delays;
+
+
+%Set parameters
+mean_interarrival = lambda;
+mean_service = 1/mu;
+num_delays_required = amount;
+
+maxSize = 100;
+
+rho = 0.0;
+%TOLEEEEE
+tole = 0.05;
+
+relativeError = zeros(1, maxSize);
+relativeError(1) = 1.0;
+relativeError(2) = 1.0;
+
+queueLength = 0.0;
+queueLengths = zeros(1, maxSize);
+meanQueueLength = 0.0;
+
+%
+Q_LIMIT = maxSize; % 100;
+
+BUSY    = 1;
+IDLE    = 0;
+
+
+
+% Especifica el numero de eventos
+num_events = 2;
+rho = mean_service * mean_interarrival;
+
+% Inicializa la simulacion
+initialize();
+i = 1;
+
+%% simulate the queue:
+while( (relativeError(i) > tole) && (i < maxSize) )
+    i = i + 1;
+    % Corre la simulacion
+    while ( num_custs_delayed < num_delays_required )
+	%fprintf(timfile, '%16.3f %d %d\n',time,num_in_q,server_status);
+	% Determina el proximo evento
+      timing();
+	% Actualiza los acumuladores estadisticos de tiempos medios
+      update_time_avg_stats();
+        % Invoca al evento apropiado
+        switch (next_event_type)
+            case 1
+                arrive();
+                %break;
+
+            case 2
+                depart();
+                %break;
+        end
+    end
+    queueLength = area_num_in_q / time;
+    % store the currente length value ...
+    queueLengths(i-1) = queueLength;
+    % calculate the mean of the queue length ...
+    meanQueueLength = mean(queueLengths(1:i-1));
+    if(i > 2)
+        %student's t analysis (t_s = 1.6) 
+	auxtrust = 2*1.6*std(queueLengths(1:i-1))/sqrt(i-1);
+	relativeError(i) = 2*auxtrust/meanQueueLength;
+        %relativeError(i) = std(queueLengths(1:i-1)) / meanQueueLength;
+    end
+    initialize();  
+end
+relativeError = relativeError(1:i);
+endfunction
+
+
+
---
+++ tp4_modelodecola/src/parte2/parte2.m
@@ -0,0 +1,55 @@
+    format long;
+    rand("seed",9);
+
+	cantSimulations = 20;
+
+	lambda = zeros(1,cantSimulations);
+	mu = zeros(1,cantSimulations);
+	meanQueueWait = zeros(1,cantSimulations);
+	rhos = zeros(1,cantSimulations);
+	theoreticalMeanQueueWait = zeros(1,cantSimulations);
+	for i = 1 : cantSimulations
+          lambda(i) = 1.0;
+          mu(i) = cantSimulations / i;
+	endfor
+	for i = 1 : cantSimulations
+	  [rho, relativeError, meanQueueTime_i] = mm1(lambda(i),mu(i),500);
+	  meanQueueLength(i) = meanQueueTime_i;
+	  rhos(i) = rho;
+	  theoreticalMeanQueueTime(i) = rho/(mu(i) - lambda(i));
+	endfor
+
+    % Plot the results
+
+    figure(1);
+
+    plot(rhos,meanQueueWait);
+
+    title(sprintf("Tiempo medio de espera en cola por cliente computado de manera empirica"));
+    xlabel(sprintf("rho"));
+    ylabel(sprintf("clientes"));
+
+    print -dpng queueWaitEmpirical.png 
+
+    figure(2);
+
+    plot(rhos,theoreticalMeanQueueWait);
+
+    title(sprintf("Tiempo medio de espera en cola por cliente computado de manera teorica"));
+    xlabel(sprintf("rho"));
+    ylabel(sprintf("clientes"));
+
+    print -dpng queueWaitTheoretical.png 
+
+    figure(3);
+
+    plot(rhos,theoreticalMeanQueueWait,"-");
+    hold on;
+    plot(rhos,meanQueueWait,"-o");
+
+    title (sprintf("Tiempo medio de espera por cliente empirico y teorico en funcion de la intensidad de trafico"));
+    xlabel (sprintf("rhos"));
+    ylabel ("clientes");
+
+    print -dpng queueWaitCompare.png
+    hold off;
