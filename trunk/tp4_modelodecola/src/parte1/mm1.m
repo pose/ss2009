8a32c15b93bf211864fa1c421626fb4898a82e10
martin.palombo <martin.palombo@8ad82c64-9a37-11de-a465-5fc2cdd6756b>
2009-11-26 13:10:19 +0000

    saco el chelazo

--- trunk/tp4_modelodecola/src/parte1/mm1.m
+++ trunk/tp4_modelodecola/src/parte1/mm1.m
@@ -52,7 +52,7 @@ mean_interarrival = lambda;
 mean_service = 1/mu;
 num_delays_required = amount;
 
-maxSize = 2000;
+maxSize = 100;
 
 rho = 0.0;
 %TOLEEEEE
@@ -127,130 +127,5 @@ while( (relativeError(i) > tole) && (i < maxSize) )
 end
 
 relativeError = relativeError(1:i);
-
-endfunction
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% Octave script: mm1.m
-% Proposito:     Simula una cola M/M/1
-%
-% Declaracion de constantes y variables:
-
-%clear all
-		  %lambda	     %mu
-function [rho,meanQueueLength] = mm1(mean_interarrival, mean_service, num_delays_required)
-
-global Q_LIMIT = 100;
-global BUSY    = 1;
-global IDLE    = 0;
-
-global next_event_type;
-global num_custs_delayed;
-global num_delays_required;
-global num_events;
-global num_in_q;
-global server_status;
-
-global area_num_in_q;
-global area_server_status;
-global mean_interarrival;
-global mean_service;
-global time;
-global time_arrival;
-global time_last_event;
-global time_next_event;
-global total_of_delays;
-
-% Especifica el numero de eventos
-num_events = 2;
-
-
-%Initialize Parameters
-maxSize = 1500;
-
-%la TOLEEEEEEE
-tole = 0.05;
-
-relativeError = zeros(1, maxSize);
-relativeError(1) = 1.0;
-relativeError(2) = 1.0;
-
-queueLength = 0.0;
-queueLengths = zeros(1, maxSize);
-meanQueueLength = 0.0;
-
-rho = mean_interarrival / mean_service;
-
-% Inicializa la simulacion
-initialize();
-
-i = 1;
-
-while( (relativeError(i) > tole) && (i < maxSize) )
-
-	% Corre la simulacion
-	while ( num_custs_delayed < num_delays_required )
-		fprintf(timfile, "%16.3f %d %d\n",time,num_in_q,server_status);
-		% Determina el proximo evento
-		timing();
-		
-		% Actualiza los acumuladores estadisticos de tiempos medios
-		update_time_avg_stats();
-		
-		% Invoca al evento apropiado
-		switch (next_event_type)
-			case 1
-				arrive();
-				%break;
-			case 2
-				depart();
-				%break;
-		endswitch
-		
-
-		queueLength = area_num_in_q / time;
-		queueLengths(i-1) = queueLength;
-		meanQueueLength = mean(queueLengths(1:i-1));
-
-		%Compute error
-		if(i > 2)
-			relativeError(i) = std(queueLengths(1:i-1)) / meanQueueLength;
-		end
-
-	endwhile
-
-endwhile
 
 endfunction
