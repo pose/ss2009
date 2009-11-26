355049594e625f77f473028450c633fde3577c95
martin.palombo <martin.palombo@8ad82c64-9a37-11de-a465-5fc2cdd6756b>
2009-11-26 04:02:35 +0000

    Cuelgaa

--- trunk/tp4_modelodecola/src/parte1/parte1.m
+++ trunk/tp4_modelodecola/src/parte1/parte1.m
@@ -1,27 +1,19 @@
-function parte1()
+    format long;
 
-	rho = 0.0;
-
-	cantSimulations = 100;
+	cantSimulations = 15;
 
 	lambda = zeros(1,cantSimulations);
 	mu = zeros(1,cantSimulations);
 	meanQueueLength = zeros(1,cantSimulations);
 	rhos = zeros(1,cantSimulations);
 	theoreticalMeanQueueLength = zeros(1,cantSimulations);
-
 	for i = 1 : cantSimulations
 		%TODO ver que ondaaeE
-		lambda(j) = 1.0;
-		mu(j) = (j+1) / i;
+		lambda(i) = 1.0;
+		mu(i) = (i+1) / cantSimulations;
 	endfor
-
 	for i = 1 : cantSimulations
-		
-		meanVals(1) = a_means(i);
-		meanVals(2) = a_means(i+1);
 		[rho, meanQueueLength_i] = mm1(lambda(i),mu(i),100);
-		
 
 		% add the empirc mean value to the array ...
 		meanQueueLength(i) = meanQueueLength_i;
@@ -32,7 +24,28 @@ function parte1()
 	%         f_mu = 1.0/meanVals(2);
 	%         dW = 1.0/(f_mu - f_lambda);
 	%         a_Lt(iCount) = f_lambda*dW;
-		theoreticalMeanQueueLength(i) = rho/(1.0 - rho);
+		theoreticalMeanQueueLength(i) = rho/(mu(i) - lambda(i));
 	endfor
 
-endfunction
+    % Plot the results
+
+    figure(1);
+
+    plot(rhos,meanQueueLength);
+
+    title(sprintf("Promedio temporal de clientes en cola en funcion de la intensidad de trafico"));
+    xlabel(sprintf("clientes"));
+    ylabel(sprintf("rho"));
+
+    print -dpng queueVSrho.png 
+
+    figure(2);
+
+    plot (rhos,meanQueueLength,strcat("o;",sprintf("Promedio temporal empirico")),rhos, theoreticalMeanQueueLength, strcat("*;",sprintf("Promedio temporal teorico")));
+
+    title (sprintf("Promedio temporal empirico y teorico en funcion de la intensidad de trafico"));
+    xlabel (sprintf("clientes"));
+    ylabel ("rho");
+
+    print -dpng teorivoVSempirico.png
+
