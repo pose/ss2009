3d96d4db8136fa74dd2ff939a9e301e5b0674789
albertopose <albertopose@8ad82c64-9a37-11de-a465-5fc2cdd6756b>
2009-11-26 20:41:54 +0000

    Ultimo commit mio (espero)

--- tp4_modelodecola/src/parte2/parte2.m
+++ tp4_modelodecola/src/parte2/parte2.m
@@ -1,7 +1,7 @@
     format long;
     rand("seed",9);
 
-	cantSimulations = 10;
+	cantSimulations = 15;
 
 	lambda = zeros(1,cantSimulations);
 	mu = zeros(1,cantSimulations);
@@ -13,43 +13,43 @@
           mu(i) = cantSimulations / i;
 	endfor
 	for i = 1 : cantSimulations
-	  [rho, relativeError, meanQueueWait_i] = mm1(lambda(i),mu(i),500);
+	  [rho, relativeError, meanQueueWait_i] = mm1(lambda(i),mu(i),100);
 	  meanQueueWait(i) = meanQueueWait_i;
 	  rhos(i) = rho;
-	  theoreticalMeanQueueTime(i) = rho/(mu(i) - lambda(i));
+	  theoreticalMeanQueueWait(i) = rho/(mu(i) - lambda(i));
 	endfor
 
     % Plot the results
 
     figure(1);
  
-    plot(rhos,meanQueueWait);
+    plot(rhos,meanQueueWait,"+");
 
-    title(sprintf("Tiempo medio de espera en cola por cliente computado de manera empirica"));
+    title(sprintf("Tiempo medio de espera en cola por cliente \ncomputado de manera empirica"));
     xlabel(sprintf("rho"));
-    ylabel(sprintf("clientes"));
+    ylabel(sprintf("unidades de tiempo"));
 
     print -dpng queueWaitEmpirical.png 
 
     figure(2);
 
-    plot(rhos,theoreticalMeanQueueWait);
+    plot(rhos,theoreticalMeanQueueWait, "o");
 
-    title(sprintf("Tiempo medio de espera en cola por cliente computado de manera teorica"));
+    title(sprintf("Tiempo medio de espera en cola por cliente \ncomputado de manera teorica"));
     xlabel(sprintf("rho"));
-    ylabel(sprintf("clientes"));
+    ylabel(sprintf("unidades de tiempo"));
 
     print -dpng queueWaitTheoretical.png 
 
     figure(3);
 
-    plot(rhos,theoreticalMeanQueueWait,"-");
+    plot(rhos,theoreticalMeanQueueWait,"-o;Teorico;");
     hold on;
-    plot(rhos,meanQueueWait,"-o");
+    plot(rhos,meanQueueWait,"+;Empirico;");
 
-    title (sprintf("Tiempo medio de espera por cliente empirico y teorico en funcion de la intensidad de trafico"));
-    xlabel (sprintf("rhos"));
-    ylabel ("clientes");
+    %title (sprintf("Tiempo medio de espera por cliente empirico y teorico\n en funcion de la intensidad de trafico"));
+    xlabel (sprintf("rho"));
+    ylabel ("unidades de tiempo");
 
     print -dpng queueWaitCompare.png
     hold off;
